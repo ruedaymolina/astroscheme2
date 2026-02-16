229124881aaa051f617bc709e19c78d68b9974a5
luis <luiscrueda.ing@gmail.com>
2026-02-15 20:28:47 -0500

    actualizar data

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,29 +1,31 @@
-import imgGrid from "./assets/inno-grid.webp";
-import imgSolar from "./assets/inno-panel.webp";
-import imgWind from "./assets/inno-wind.webp";
+import { imgGrid, imgSolar,imgWind } from "./images";
 
-export const innovationsSection = {
-  title: "Latest Innovations",
-  subheading1_1: "Discover Our Cutting-edge",
-  subheading1_2: " Green Energy Technologies",
+
+
+export const innovationsSection1 = {
+  title: "22222.Latest Innovations",
+  subheading1_1: "22.Discover Our Cutting-edge",
+  subheading1_2: " 22.Green Energy Technologies",
   innovations: [
     {
-      title: "Smart Grid Integration",
+      title: "1.Smart Grid Integration",
       description:
         "Revolutionize energy distribution with our smart grid technology, optimizing efficiency, minimizing wastage, and adapting to demand fluctuations. Stay connected with innovative solutions for a sustainable, cost-effective energy ecosystem. Embrace the future with our cutting-edge smart grid integration, enhancing reliability and paving the way for efficiency and sustainability.",
-      visual: imgGrid,
+       visual: imgGrid,
     },
     {
-      title: "Advanced Solar Panel Design",
+      title: "1.Advanced Solar Panel Design",
       description:
         "Boost solar efficiency with our state-of-the-art solar panel designs. Our innovative technologies capture more sunlight, ensuring maximum energy production for your home or business. Experience enhanced sustainability and increased energy output with our advanced solar solutions.",
-      visual: imgSolar,
+       visual: imgSolar,
+      
+      
     },
     {
-      title: "Next-gen Wind Turbines",
+      title: "1.Next-gen Wind Turbines",
       description:
         "Transform your wind energy experience with our next-generation wind turbines. Our sleek design, enhanced efficiency, and minimal environmental impact underscore our commitment to innovation. Elevate your sustainable energy journey with cutting-edge wind solutions that prioritize both performance and environmental responsibility.",
-      visual: imgWind,
+     visual: imgWind,
     },
   ],
   visual: "innovations-section-image.jpg",
---
+++ src/constants/images.ts
@@ -0,0 +1,9 @@
+import imgGrid from "../assets/inno-grid.webp";
+import imgSolar from "../assets/inno-panel.webp";
+import imgWind from "../assets/inno-wind.webp";
+
+export {
+    imgGrid,
+    imgSolar,
+    imgWind
+}
